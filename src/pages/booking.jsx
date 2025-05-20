beb6b2c9bebb74718cf505ed100db33236f464e2
unknown <sajithroshitha98@gmail.com>
2025-05-20 07:31:06 +0530

    update booking ui

--- src/pages/booking.jsx
+++ src/pages/booking.jsx
@@ -48,7 +48,7 @@ function Booking() {
             </div>
 
             {/* Check-out */}
-            <div className="mb-4 field-wrp">
+            <div className="field-wrp">
               <label className="checkout-label input-label">Check-out Date</label>
               <DatePicker
                 selected={checkOutDate}
@@ -63,7 +63,7 @@ function Booking() {
             </div>
 
             {/* Vehicle Type */}
-            <div className="mb-6 field-wrp">
+            <div className="field-wrp">
               <label className="vehicle-type input-label">Vehicle Type</label>
 
               <Select
